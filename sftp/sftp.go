feddba3b23d62fbabf5c65cc5850e18544b9bd88
陶辰 <chen.tao@ifchange.com>
2020-04-11 21:15:03 +0800

    update

--- sftp/sftp.go
+++ sftp/sftp.go
@@ -10,13 +10,20 @@ import (
 )
 
 type Ssh struct {
-	User     string `json:"user"`
-	Password string `json:"password"`
-	Host     string `json:"host"`
-	Port     int    `json:"port"`
+	User       string       `json:"user"`
+	Password   string       `json:"password"`
+	Host       string       `json:"host"`
+	Port       int          `json:"port"`
+	sftpClient *sftp.Client `json:"sftp_client"`
 }
 
-func (params *Ssh) Connect() (*sftp.Client, error) {
+// UserInterface is test interface.
+type SshInterface interface {
+	Connect()
+	SendFile(localFilePath, remoteDir string) error
+}
+
+func (params *Ssh) Connect() error {
 	var (
 		auth         []ssh.AuthMethod
 		addr         string
@@ -35,23 +42,23 @@ func (params *Ssh) Connect() (*sftp.Client, error) {
 		Timeout: 30 * time.Second,
 	}
 
-	// connet to ssh
+	// connect to ssh
 	addr = fmt.Sprintf("%s:%d", params.Host, params.Port)
 
 	if sshClient, err = ssh.Dial("tcp", addr, clientConfig); err != nil {
-		return nil, err
+		return err
 	}
 
 	// create sftp client
 	if sftpClient, err = sftp.NewClient(sshClient); err != nil {
-		return nil, err
+		return err
 	}
-
-	return sftpClient, nil
+	params.sftpClient = sftpClient
+	return nil
 }
 
-func SendFile(sftpClient *sftp.Client, localFilePath, remoteDir string) error {
-	defer sftpClient.Close()
+func (params *Ssh) SendFile(localFilePath, remoteDir string) error {
+	defer params.sftpClient.Close()
 	srcFile, err := os.Open(localFilePath)
 	if err != nil {
 		return err
@@ -59,7 +66,7 @@ func SendFile(sftpClient *sftp.Client, localFilePath, remoteDir string) error {
 	defer srcFile.Close()
 
 	var remoteFileName = path.Base(localFilePath)
-	dstFile, err := sftpClient.Create(path.Join(remoteDir, remoteFileName))
+	dstFile, err := params.sftpClient.Create(path.Join(remoteDir, remoteFileName))
 	if err != nil {
 		return err
 	}
